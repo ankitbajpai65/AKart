0fc10f8e7fb38dfd2101499cc912b1d6d222b34e
Ankit Bajpai <ankit65bajpai@gmail.com>
2024-07-21 13:53:10 +0530

    style:some minor changes

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,128 +1,176 @@
-import React, { useState, useEffect } from 'react';
-import '../style/Contact.css';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { auth } from './firebase';
+import React, { useState, useEffect } from "react";
+import "../style/Contact.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { auth } from "./firebase";
 
 const Contact = () => {
-    const [name, setName] = useState(false);
-    const [email, setEmail] = useState(false);
-    const [msg, setMsg] = useState(false);
-    const [detail, setDetail] = useState({
-        fullname: "",
-        email: "",
-        message: ""
-    });
-    // trying to autofill name and email if user is in login state.
-    // useEffect(() => {
-    //     auth.onAuthStateChanged((user) => {
-    //         console.log(user);
-    //         if (user) {
-    //             detail.fullname = user.displayName;
-    //             detail.email = user.email;
-    //         }
-    //     })
+  const [name, setName] = useState(false);
+  const [email, setEmail] = useState(false);
+  const [msg, setMsg] = useState(false);
+  const [detail, setDetail] = useState({
+    fullname: "",
+    email: "",
+    message: "",
+  });
+  // trying to autofill name and email if user is in login state.
+  // useEffect(() => {
+  //     auth.onAuthStateChanged((user) => {
+  //         console.log(user);
+  //         if (user) {
+  //             detail.fullname = user.displayName;
+  //             detail.email = user.email;
+  //         }
+  //     })
+  // })
+
+  const inputEvent = (event) => {
+    // console.log(event.target.name, event.target.value);
+    const value = event.target.value;
+    const name = event.target.name;
+    // setDetail((prev) => {
+    //     return {
+    //         ...prev,
+    //         [name]: value
+    //     }
     // })
-    const inputEvent = (event) => {
-        // console.log(event.target.name, event.target.value);
-        const value = event.target.value;
-        const name = event.target.name;
-        // setDetail((prev) => {
-        //     return {
-        //         ...prev,
-        //         [name]: value
-        //     }
-        // })
-        setDetail({ ...detail, [name]: value });
-    }
-    const validateName = () => {
-        let reg1 = /^[a-zA-Z ]+$/;
-        if (!reg1.test(detail.fullname)) {
-            setName(true);
-            return false;
-        }
-        else {
-            if (detail.fullname.length <= 2) {
-                setName(true);
-                return false;
-            }
-        }
-        setName(false);
-    }
-    const validateMail = () => {
-        let reg2 = /^[a-zA-Z0-9._]+@+[a-zA-Z0-9._]+\.[a-zA-Z]{2,4}$/;
-        if (!reg2.test(detail.email)) {
-            setEmail(true);
-            return false;
-        }
-        setEmail(false);
-    }
-    const validateMsg = () => {
-        if (detail.message.length == "") {
-            setMsg(true);
-            return false;
-        }
-        setMsg(false);
-    }
-    const postData = async (event) => {
-        event.preventDefault();
-        const { fullname, email, message } = detail;
-        if (fullname && email && message) {
-            const res = await fetch('https://e-commerce-website-a04e4-default-rtdb.firebaseio.com/contactForm.json',
-                {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-                    body: JSON.stringify({
-                        fullname,
-                        email,
-                        message
-                    })
-                })
-            toast.success("Your form has been submitted successfully!", {
-                position: "top-center",
-                theme: "dark"
-            });
-            function removeVal() {
-                document.querySelectorAll('.details').forEach(element => {
-                    element.value = "";
-                });
-            }
-            setTimeout(removeVal, 500);
-            setDetail(null);
-        }
-        else {
-            toast.error("Please fill all details!", {
-                position: "top-center",
-                theme: "dark"
-            });
-            return false;
-        }
-    }
-    return (
-        <>
-            <div className="contact">
-                <div className="container-fluid contactDiv d-flex align-items-center justify-content-center">
-                    <h1>Contact Us</h1>
-                </div>
-                <div id="formDiv" className="d-flex flex-column justify-content-center align-items-center">
-                    <form action="" className="form container" method="POST">
-                        {name && <span className="errorMsg row mb-3">*Must hold alphabets only and length should be greater than 2</span>}
-                        <input type="text" id="name" className={`${name ? 'notifyError details input row' : 'details input row'}`} onChange={inputEvent} onBlur={validateName} name="fullname" placeholder="Enter name" />
+    setDetail({ ...detail, [name]: value });
+  };
 
-                        {email && <span className="errorMsg row mb-3">*Please enter valid email!</span>}
-                        <input type="" id="email" className="details input row" onChange={inputEvent} onBlur={validateMail} name="email" placeholder="Enter email" />
+  const validateName = () => {
+    let reg1 = /^[a-zA-Z ]+$/;
+    if (!reg1.test(detail.fullname)) {
+      setName(true);
+      return false;
+    } else {
+      if (detail.fullname.length <= 2) {
+        setName(true);
+        return false;
+      }
+    }
+    setName(false);
+  };
 
-                        {msg && <span className="errorMsg row mb-3">*Message is required!</span>}
-                        <textarea rows="5" cols="20" id="message" className="details row" onChange={inputEvent} onBlur={validateMsg} name="message" placeholder="Your message" />
+  const validateMail = () => {
+    let reg2 = /^[a-zA-Z0-9._]+@+[a-zA-Z0-9._]+\.[a-zA-Z]{2,4}$/;
+    if (!reg2.test(detail.email)) {
+      setEmail(true);
+      return false;
+    }
+    setEmail(false);
+  };
 
-                        <button className="btn btn-secondary" onClick={postData}>Submit</button>
-                    </form>
-                </div>
-            </div>
-            <ToastContainer />
-        </>
-    );
-}
+  const validateMsg = () => {
+    if (detail.message.length == "") {
+      setMsg(true);
+      return false;
+    }
+    setMsg(false);
+  };
+
+  const postData = async (event) => {
+    event.preventDefault();
+    const { fullname, email, message } = detail;
+    if (fullname && email && message) {
+      const res = await fetch(
+        "https://e-commerce-website-a04e4-default-rtdb.firebaseio.com/contactForm.json",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            fullname,
+            email,
+            message,
+          }),
+        }
+      );
+      toast.success("Your form has been submitted successfully!", {
+        position: "top-center",
+        theme: "dark",
+      });
+      function removeVal() {
+        document.querySelectorAll(".details").forEach((element) => {
+          element.value = "";
+        });
+      }
+      setTimeout(removeVal, 500);
+      setDetail(null);
+    } else {
+      toast.error("Please fill all details!", {
+        position: "top-center",
+        theme: "dark",
+      });
+      return false;
+    }
+  };
+
+  return (
+    <>
+      <div className="contact">
+        <div className="container-fluid contactDiv d-flex align-items-center justify-content-center">
+          <h1>Contact Us</h1>
+        </div>
+        <div
+          id="formDiv"
+          className="d-flex flex-column justify-content-center align-items-center"
+        >
+          <form action="" className="form container" method="POST">
+            {name && (
+              <span className="errorMsg row mb-3">
+                *Must hold alphabets only and length should be greater than 2
+              </span>
+            )}
+            <input
+              type="text"
+              id="name"
+              className={`${
+                name ? "notifyError details input row" : "details input row"
+              }`}
+              onChange={inputEvent}
+              onBlur={validateName}
+              name="fullname"
+              placeholder="Enter name"
+            />
+
+            {email && (
+              <span className="errorMsg row mb-3">
+                *Please enter valid email!
+              </span>
+            )}
+            <input
+              type=""
+              id="email"
+              className="details input row"
+              onChange={inputEvent}
+              onBlur={validateMail}
+              name="email"
+              placeholder="Enter email"
+            />
+
+            {msg && (
+              <span className="errorMsg row mb-3">*Message is required!</span>
+            )}
+            <textarea
+              rows="5"
+              cols="20"
+              id="message"
+              className="details row"
+              onChange={inputEvent}
+              onBlur={validateMsg}
+              name="message"
+              placeholder="Your message"
+            />
+
+            <button className="btn btn-secondary" onClick={postData}>
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+      <ToastContainer />
+    </>
+  );
+};
 export default Contact;
--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,111 +1,138 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import '../style/Login.css';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "../style/Login.css";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
-import { auth } from './firebase';
-import VisibilityIcon from '@material-ui/icons/Visibility';
-import VisibilityOffIcon from '@material-ui/icons/VisibilityOff';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { auth } from "./firebase";
+import VisibilityIcon from "@material-ui/icons/Visibility";
+import VisibilityOffIcon from "@material-ui/icons/VisibilityOff";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Signup = () => {
-    const navigate = useNavigate();
-    const [invalidMail, setInvalidMail] = useState()
-    const [invalidPass, setInvalidPass] = useState()
-    const [user, setUser] = useState({
-        fullname: "",
-        email: "",
-        pass: ""
-    })
-    const [submitBtnDisable, setSubmitBtnDisable] = useState(false);
-    const [visible, setVisible] = useState(false);
-    const loginClicked = () => {
-        navigate('/login');
+  const navigate = useNavigate();
+  const [invalidMail, setInvalidMail] = useState();
+  const [invalidPass, setInvalidPass] = useState();
+  const [user, setUser] = useState({
+    fullname: "",
+    email: "",
+    pass: "",
+  });
+  const [submitBtnDisable, setSubmitBtnDisable] = useState(false);
+  const [visible, setVisible] = useState(false);
+  const loginClicked = () => {
+    navigate("/login");
+  };
+
+  const visibleClicked = () => {
+    setVisible(true);
+  };
+
+  const notVisibleClicked = () => {
+    setVisible(false);
+  };
+
+  const inputEvent = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    // console.log(name, value);
+    setUser({ ...user, [name]: value });
+  };
+
+  const handleSubmission = () => {
+    if (!user.fullname || !user.email || !user.pass) {
+      // alert("Please fill all entries!");
+      toast.error("Please fill all details!", {
+        position: "top-center",
+        theme: "dark",
+      });
+      return;
     }
-    const inputEvent = (e) => {
-        const name = e.target.name;
-        const value = e.target.value;
-        // console.log(name, value);
-        setUser({ ...user, [name]: value });
+    console.log(user);
+    setSubmitBtnDisable(true);
+    createUserWithEmailAndPassword(auth, user.email, user.pass)
+      .then(async (res) => {
+        setSubmitBtnDisable(false);
+        const x = res.user;
+        await updateProfile(x, {
+          displayName: user.fullname,
+        });
+        console.log(res);
+        navigate("/");
+      })
+      .catch((err) => {
+        setSubmitBtnDisable(false);
+        // console.log("error is", err)
+        // alert(err.message);
+        if (err.message === "Firebase: Error (auth/invalid-email).")
+          setInvalidMail("The email id is invalid!");
+        if (err.message === "Firebase: Error (auth/email-already-in-use).")
+          setInvalidMail("Email already in use!");
+        if (
+          err.message ===
+          "Firebase: Password should be at least 6 characters (auth/weak-password)."
+        )
+          setInvalidPass("Password should be at least 6 characters!");
+        setTimeout(() => {
+          setInvalidMail();
+          setInvalidPass();
+        }, 2000);
+      });
+    function removeVal() {
+      document.querySelectorAll("input").forEach((element) => {
+        element.value = "";
+      });
     }
-    const handleSubmission = () => {
-        if (!user.fullname || !user.email || !user.pass) {
-            // alert("Please fill all entries!");
-            toast.error("Please fill all details!", {
-                position: "top-center",
-                theme: "dark"
-            });
-            return;
-        }
-        console.log(user);
-        setSubmitBtnDisable(true);
-        createUserWithEmailAndPassword(auth, user.email, user.pass).then(
-            async (res) => {
-                setSubmitBtnDisable(false);
-                const x = res.user;
-                await updateProfile(x, {
-                    displayName: user.fullname
-                })
-                console.log(res);
-                navigate('/');
-            }).catch((err) => {
-                setSubmitBtnDisable(false);
-                // console.log("error is", err)
-                // alert(err.message);
-                if (err.message === "Firebase: Error (auth/invalid-email).")
-                    setInvalidMail("The email id is invalid!")
-                if (err.message === "Firebase: Error (auth/email-already-in-use).")
-                    setInvalidMail("Email already in use!")
-                if (err.message === "Firebase: Password should be at least 6 characters (auth/weak-password).")
-                    setInvalidPass("Password should be at least 6 characters!")
-                setTimeout(() => {
-                    setInvalidMail()
-                    setInvalidPass()
-                }, 2000)
-            });
-        function removeVal() {
-            document.querySelectorAll('input').forEach(element => {
-                element.value = "";
-            });
-        }
-        if (!setInvalidMail && !setInvalidPass)
-            setTimeout(removeVal, 500);
-    }
-    const visibleClicked = () => {
-        setVisible(true);
-    }
-    const notVisibleClicked = () => {
-        setVisible(false);
-    }
-    return (
-        <>
-            <div className="loginSec">
-                <div className='loginDiv'>
-                    <div className="loginFormDiv">
-                        <label>Name</label>
-                        <input type="text" onChange={inputEvent} name="fullname" />
-                        <label>Email</label>
-                        <input type="text" onChange={inputEvent} name="email" />
-                        <span className="errorMsg text-danger fw-semibold d-block">{invalidMail}</span>
-                        <label>Password</label>
-                        <input type={visible ? "text" : "password"} onChange={inputEvent} name="pass" />
-                        <span className="errorMsg text-danger fw-semibold d-block">{invalidPass}</span>
-                        {visible ?
-                            <VisibilityOffIcon className="vIcon" onClick={notVisibleClicked} />
-                            :
-                            <VisibilityIcon className="vIcon" onClick={visibleClicked} />
-                        }
-                        <button className="btn btn-success w-100 loginPageBtn" onClick={handleSubmission} disabled={submitBtnDisable}>Signup</button>
-                        <div className="text-center not_has_account">
-                            <span>Already have an account? </span>
-                            <button className="btn text-success mb-2" onClick={loginClicked}>Login</button>
-                        </div>
-                    </div>
-                </div>
-            </div >
-            <ToastContainer />
-        </>
-    );
-}
+    if (!setInvalidMail && !setInvalidPass) setTimeout(removeVal, 500);
+  };
+
+  return (
+    <>
+      <div className="loginSec">
+        <div className="loginDiv">
+          <div className="loginFormDiv">
+            <label>Name</label>
+            <input type="text" onChange={inputEvent} name="fullname" />
+            <label>Email</label>
+            <input type="text" onChange={inputEvent} name="email" />
+            <span className="errorMsg text-danger fw-semibold d-block">
+              {invalidMail}
+            </span>
+            <label>Password</label>
+            <input
+              type={visible ? "text" : "password"}
+              onChange={inputEvent}
+              name="pass"
+            />
+            <span className="errorMsg text-danger fw-semibold d-block">
+              {invalidPass}
+            </span>
+            {visible ? (
+              <VisibilityOffIcon
+                className="vIcon"
+                onClick={notVisibleClicked}
+              />
+            ) : (
+              <VisibilityIcon className="vIcon" onClick={visibleClicked} />
+            )}
+            <button
+              className="btn btn-success w-100 signupBtn"
+              onClick={handleSubmission}
+              disabled={submitBtnDisable}
+            >
+              Signup
+            </button>
+            <div className="text-center not_has_account">
+              <span>Already have an account? </span>
+              <button className="btn text-success mb-2" onClick={loginClicked}>
+                Login
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <ToastContainer />
+    </>
+  );
+};
 
 export default Signup;
